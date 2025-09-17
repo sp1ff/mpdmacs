e11d46925ce711de37352fb0a243a2cb55f873a3
Michael Herstine <sp1ff@pobox.com>
2025-09-16 17:30:51 -0700

    Cut release 1.0.1

--- mpdmacs.el
+++ mpdmacs.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020-2025 Michael Herstine <sp1ff@pobox.com>
 
 ;; Author: Michael Herstine <sp1ff@pobox.com>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((emacs "29.1") (elmpd "0.3"))
 ;; Keywords: comm
 ;; URL: https://github.com/sp1ff/mpdmacs
@@ -58,7 +58,7 @@
 (require 'cl-lib)
 (require 'elmpd)
 
-(defconst mpdmacs-version "1.0.0")
+(defconst mpdmacs-version "1.0.1")
 
 (defgroup mpdmacs nil
   "A lightweight MPD client for Emacs."
